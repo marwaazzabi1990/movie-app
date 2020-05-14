a7b63beacac0cd62ace5e931ebe183cdbeaad00b
marwa azzabi <marwaazzabi90@gmail.com>
2020-05-14 14:24:07 +0100

    movieapp with rating

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,111 +1,131 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
-import Moviei from './component/movies/Moviei'
-import Navbar from './component/Navbar/Navbar'
-import cover1 from './component/movies/cover1.jpeg'
-import cover2 from './component/movies/cover2.jpeg'
+import Moviei from "./component/movies/Moviei";
+import Navbar from "./component/Navbar/Navbar";
+import cover1 from "./component/movies/cover1.jpeg";
+import cover2 from "./component/movies/cover2.jpeg";
 
-import Sigin from './component/sign/sigin'
-import Register from './component/sign/Register'
+import Sigin from "./component/sign/sigin";
+import Register from "./component/sign/Register";
 import { AiTwotoneStar } from "react-icons/ai";
-import Detail from './component/movies/movieDetai'
-import Favorie from './component/favorie/favorie'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-
-
-
+import Detail from "./component/movies/movieDetai";
+import Favorie from "./component/favorie/favorie";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 export default class App extends Component {
-  state={
- favorie:[]
-  }
+  state = {
+    favorie: [],
+  };
   movie = [
-   
-    {id:1,name:"batman",description:"Lorem Ipsum est un texte d'espace réservé ",rating:"*",categorie:"Documentary" ,img:cover1},
-    {id:2,name:"superman",description:"Lorem Ipsum est un texte d'espace réservé",rating:"*",categorie:"Action",img:cover2 },
-    
-   
-      ]
-     
-    addFavorie= (e) => {
- alert('JE COMMENCE')
-             this.state.favorie.push(e)
+    {
+      id: 1,
+      name: "hhhhhhhh",
+      description: "Lorem Ipsum est un texte d'espace réservé ",
+      rating: "****",
+      categorie: "Documentary",
+      img: cover1,
+    },
+    {
+      id: 2,
+      name: "superman",
+      description: "Lorem Ipsum est un texte d'espace réservé",
+      rating: "*",
+      categorie: "Action",
+      img: cover2,
+    },
+    {
+      id: 3,
+      name: "batman",
+      description: "Lorem Ipsum est un texte d'espace réservé ",
+      rating: "**",
+      categorie: "Documentary",
+      img: cover1,
+    },
+    {
+      id: 4,
+      name: "superman",
+      description: "Lorem Ipsum est un texte d'espace réservé",
+      rating: "***",
+      categorie: "Action",
+      img: cover2,
+    },
+    {
+      id: 5,
+      name: "superman",
+      description: "Lorem Ipsum est un texte d'espace réservé",
+      rating: "*****",
+      categorie: "Action",
+      img: cover2,
+    },
+  ];
 
+  addFavorie = (e) => {
+    alert("Ce film bien ajouter au favorie");
+    this.state.favorie.push(e);
+  };
+  remove = (e) => {
+    alert("supprime");
+    let a = "";
+    let c = [];
+    for (let i = 0; i < this.state.favorie.length; i++) {
+      if (e == this.state.favorie[i]) {
+        a += i;
+        c = [...this.state.favorie.splice(1, 1)];
+      }
+    }
+    this.setState({
+      favorie: c,
+    });
+  };
+  /*rating*/
 
-                     }
-  remove=(e)=>{
-    alert('supprime')
-  let a="";
-  let c=[];
-   for(let i=0 ;i<this.state.favorie.length;i++)
-   {
-     if(e==this.state.favorie[i])
-     {
-       a+=i
-    c =[...this.state.favorie.splice(1,1)]
-      
-     
-     }
-   }
-   this.setState({
-    favorie:c
-  })
-  
-    
-  }
-                     
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <div>
+                <Navbar />
+              </div>
 
-  componentDidUpdate(nextProps,nextState){
-    localStorage.setItem('user','JSON.stringify(nextState,user)')
-  
-  }
-  render(){
-  return (
-    <div className="App" >
-      
-      
-      <Router>
-      <Switch>
-      <Route  exact  path='/'>
-           <div>< Navbar/></div>
-           
-          <div  ><Moviei  movie={this.movie} addFavorie={this.addFavorie} /></div> 
-          </Route>
-         
-     <Route  path='/login'>
-        <div>< Navbar/></div>
-          <div>  <Sigin /></div> 
-          </Route>
-          <Route path='/register'>
-          <div>< Navbar/></div>
-          <div> <Register /></div> 
-          </Route>
-         {this.movie.map((el) => (<Route  exact path={"/detail/"+el.id} >
-          
-         <Detail el={el} />
-         </Route>))}
-         
-        <Route>
-          
-         <Favorie favorie={this.state.favorie} remove={this.remove} />
-         </Route>
-       
-      
+              <div>
+                <Moviei movie={this.movie} addFavorie={this.addFavorie} />
+              </div>
+            </Route>
 
-      
-      
-        </Switch>
+            <Route path="/login">
+              <div>
+                <Navbar />
+              </div>
+              <div>
+                {" "}
+                <Sigin />
+              </div>
+            </Route>
+            <Route path="/register">
+              <div>
+                <Navbar />
+              </div>
+              <div>
+                {" "}
+                <Register />
+              </div>
+            </Route>
+            {this.movie.map((el) => (
+              <Route exact path={"/detail/" + el.id}>
+                <Detail el={el} />
+              </Route>
+            ))}
+
+            <Route>
+              <Favorie favorie={this.state.favorie} remove={this.remove} />
+            </Route>
+          </Switch>
         </Router>
-
-    
-    </div>
-  );
-}}
-
+      </div>
+    );
+  }
+}
--- movieapp/src/component/movies/Moviei.js
+++ movieapp/src/component/movies/Moviei.js
@@ -1,184 +1,193 @@
-import React, { Component } from 'react';
-import './movie.css'
-import Member from  './movei'
-import cover1 from './cover1.jpeg'
-import { Button,ButtonToolbar} from 'react-bootstrap';
-import ButtonToolBar  from 'react-bootstrap/'
-import {AddMoviesModal} from './AddMovieModal'
+import React, { Component } from "react";
+import "./movie.css";
+import Member from "./movei";
+import cover1 from "./cover1.jpeg";
+import { Button, ButtonToolbar } from "react-bootstrap";
+import ButtonToolBar from "react-bootstrap/";
+import { AddMoviesModal } from "./AddMovieModal";
 
-import Detail from './movieDetai'
-import AddMovie from './addmovie'
+import Detail from "./movieDetai";
+import AddMovie from "./addmovie";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-    const users=[
-      {login:'marwa',pasword:'123'},
-      {login:'nour',pasword:'1234'}
-       
-    ]
-
-
-
+const users = [
+  { login: "marwa", pasword: "123" },
+  { login: "nour", pasword: "1234" },
+];
 
 export default class Moviei extends Component {
-  state={
-    Caractere: '',
-    movies :this.props.movie,
+  state = {
+    Caractere: "",
+    movies: this.props.movie,
     name: "",
-    addModelShow : false,
-    
-    user:[]
-  }
-  favorie=[
-    {id:1,name:"batman",description:"Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique",rating:"*",categorie:"Documentary" ,img:cover1},
+    addModelShow: false,
 
-  ]
-  
-/*ADD MOVIE*/
+    table: [],
+    ratebtn: [1, 2, 3, 4, 5],
+    rate: 0,
+    etat: false,
+  };
+  favorie = [
+    {
+      id: 1,
+      name: "batman",
+      description:
+        "Lorem Ipsum est un texte d'espace réservé couramment utilisé dans les industries graphique",
+      rating: "*",
+      categorie: "Documentary",
+      img: cover1,
+    },
+  ];
+
+  /*ADD MOVIE*/
   handleChangeName = (value) => {
     this.setState({
       name: value,
     });
-
   };
   handleChangeDescription = (value) => {
     this.setState({
       description: value,
     });
-
   };
   handleChangeimg = (value) => {
     this.setState({
       img: value,
     });
-
   };
   handleChangeRate = (value) => {
     this.setState({
       rate: value,
     });
-
   };
   addObjectMovie = (e) => {
-   this.props.movie.push({
+    this.props.movie.push({
       name: this.state.name,
       description: this.state.description,
       rating: this.state.rating,
-      img:this.state.img
+      img: this.state.img,
     });
     console.log(this.props.movie);
-    this.setState({ description: "",name: "" ,rating: ""  });
+    this.setState({ description: "", name: "", rating: "" });
   };
 
-
-
-
   /*SEARCH FUNCTION*/
-  SearchBar =(e)=>{
-    let movies = this.state.movies
-   
-  if(e.target.value.length===0){
-    
+  SearchBar = (e) => {
+    let movies = this.state.movies;
 
-    this.setState({movies:this.props.movie})
-     
-  }
-  else
-  {
-    this.setState({Caractere: e.target.value})
-     
-      let k = this.state.Caractere
-     
+    if (!e.target.value) {
+      this.setState({ movies: this.props.movie });
+    } else {
+      this.setState({ Caractere: e.target.value });
 
-   movies = movies.filter(el => el.name.includes(k))
-  
-  
-  this.setState({movies:movies})
-  
+      let k = this.state.Caractere;
 
-  }
-     
-  }
+      movies = movies.filter((el) => el.name.includes(k));
 
-  
+      this.setState({ movies: movies });
+    }
+  };
+  /*rating*/
+  getrate = (e) => {
+    this.state.etat = true;
+    let movies = this.state.movies;
+    this.setState({
+      rate: e,
+    });
+    console.log(this.state.rate);
+    console.log(e);
+    this.state.table = this.state.movies.filter((el) => el.rating.length >= e);
 
-render(){
-  let  addModelClose=()=>this.setState({addModelShow:false})
-  let  addModelClose1=()=>this.setState({addModelShow1:false})
-  return (
-    <div>
-     
-      <div className="zone-recherche">
-           <div  >
-           <input className="recherchezone" type="text" placeholder="Search..."  onChange={this.SearchBar}/>
-         
-         </div>
-         <div className="rating">
-          <a className="etoile1" > <i   className="fas fa-star"></i></a>
-          <a className="etoile">  <i className="fas fa-star"></i></a>
-         <a className="etoile"><i className="fas fa-star"></i></a> 
-         <a className="etoile">    <i className="fas fa-star"></i></a>
-       <a className="etoile"> <i className="fas fa-star"></i></a> </div>
-         </div>
-       
-  
-    <h1 className="title">Movie library</h1>
-    <div className="cards " >
-  
-           {this.state.movies.map(el => 
-           <div className="card " key={el.id}>
-            
-                <img  className="card img " src={el.img}/><br/>
-                   <div className="card_title">
-                    
-                      <h3>{el.name}</h3>
-                      <span>{el.rating}</span>
-                      <p>{el.description}</p></div>
-         
-                        <div className="btn-card">
-                      <Link to={"/detail/"+el.id} >  <button className="btn-card-detail" >More Detail</button></Link> 
-            
-                         <button  className="btn-card-detail"  onClick={()=>this.props.addFavorie(el)}>Add Favori</button>
-             
-                        
+    this.affichage(this.state.etat);
+  };
+  affichage = (et) => {
+    if (this.state.etat == true) {
+      alert("true");
+      this.setState({
+        movies: this.state.table,
+      });
+    } else {
+      alert("false");
+      this.setState({
+        movies: this.props.movie,
+      });
+    }
+  };
+
+  render() {
+    // let condition = this.state.rate ? table : this.state.movies;
+
+    let addModelClose = () => this.setState({ addModelShow: false });
+    let addModelClose1 = () => this.setState({ addModelShow1: false });
+    return (
+      <div>
+        <div className="zone-recherche">
+          <div>
+            <input
+              className="recherchezone"
+              type="text"
+              placeholder="Search..."
+              onChange={this.SearchBar}
+            />
+          </div>
+          <div className="rating">
+            {this.state.ratebtn.map((el) => (
+              <button onClick={() => this.getrate(el)}>
+                <i style={{ color: "red" }} className="fas fa-star"></i>
+              </button>
+            ))}{" "}
+          </div>
+        </div>
+
+        <h1 className="title">Movie library</h1>
+        <div className="cards ">
+          {this.state.movies.map((el) => (
+            <div className="card " key={el.id}>
+              <img className="card img " src={el.img} />
+              <br />
+              <div className="card_title">
+                <h3>{el.name}</h3>
+                <span>{el.rating}</span>
+                <p>{el.description}</p>
               </div>
-              </div> )} 
-        
-           
-    </div><br/>
-    <ButtonToolbar>
-       <button  className="buttonplus" onClick={()=>this.setState({addModelShow:true})}>Add Film</button></ButtonToolbar>
-        <AddMoviesModal 
-                  addfilmName={this.handleChangeName}
-                  addfilmDescription={this.handleChangeDescription}
-                  addfilmRate={this.handleChangeRate}
-                  addfilmimg={this.handleChangeimg}
-                  addObjectMovie={this.addObjectMovie}
-                  state={this.state}
-                  show={this.state.addModelShow}
-                  onHide={addModelClose} 
-                />
-       
-   
- 
 
+              <div className="btn-card">
+                <Link to={"/detail/" + el.id}>
+                  {" "}
+                  <button className="btn-card-detail">More Detail</button>
+                </Link>
 
-    
-           
-    
-  
-     
-</div>
-
-
-    
-  );
-}
-
+                <button
+                  className="btn-card-detail"
+                  onClick={() => this.props.addFavorie(el)}
+                >
+                  Add Favori
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <br />
+        <ButtonToolbar>
+          <button
+            className="buttonplus"
+            onClick={() => this.setState({ addModelShow: true })}
+          >
+            Add Film
+          </button>
+        </ButtonToolbar>
+        <AddMoviesModal
+          addfilmName={this.handleChangeName}
+          addfilmDescription={this.handleChangeDescription}
+          addfilmRate={this.handleChangeRate}
+          addfilmimg={this.handleChangeimg}
+          addObjectMovie={this.addObjectMovie}
+          state={this.state}
+          show={this.state.addModelShow}
+          onHide={addModelClose}
+        />
+      </div>
+    );
+  }
 }
